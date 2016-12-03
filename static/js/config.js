e90dea5ce9a52cb84f92fb2e38c38d7f57338c1f
Minke Zhang <minke.zhang@gmail.com>
2016-12-02 19:50:15 -0800

    add forwarding, init calls

---
+++ static/js/config.js
@@ -0,0 +1,10 @@
+var key = "dnu57xap0ysyvi";
+var config = {
+  key: key,
+  config: {
+    "iceServers": [
+        { url: "stun:stun.l.google.com:19302" },
+        { url: "turn:numb.viagenie.ca", username: "webrtc@live.com", credential: "muazkh" }
+    ]
+  }
+};
